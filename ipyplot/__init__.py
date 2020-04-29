3d52d25b9c46480d0ee88c21d098780c645a7dc9
Karol Zak <karol.zak@hotmail.com>
2020-04-29 13:40:42 +0200

    add setup and init.py

---
+++ ipyplot/__init__.py
@@ -0,0 +1,5 @@
+from .plotting import (
+    plot_class_representations, plot_class_tabs,
+    plot_images
+)
+import ipyplot.img_helpers
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="ipyplot",
+    version="0.0.1",
+    description="Simple package that leverages IPython and HTML for more efficient, reach and interactive plotting of images in Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # This is important!
+    url="http://github.com/karolzak/ipyplot",
+    author="Karol Zak",
+    author_email="karol.zak@hotmail.com",
+    license="MIT",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+)
