09a77b7bb4a2ebd8a3f552cc2856f6e6e9330aa3
Karol Zak <karol.zak@hotmail.com>
2020-05-06 11:28:18 +0200

    fix imports

--- ipyplot/__init__.py
+++ ipyplot/__init__.py
@@ -1,10 +1,5 @@
 import sys
 
-try:
-    from IPython.display import display, HTML
-except Exception as e:
-    raise Exception('IPython not detected. Plotting without IPython is not possible')  # NOQA E501
-
 if 'google.colab' in sys.modules:
     print("WARNING! You might encounter some issues while running in Google colab environment")  # NOQA E501
 
