b1a0a533f3ae4b31c51ba41717f91b8c049d6204
Karol Zak <karol.zak@hotmail.com>
2020-05-06 11:00:02 +0200

    add check for google colab in init.py

--- ipyplot/__init__.py
+++ ipyplot/__init__.py
@@ -1,5 +1,16 @@
+import sys
+
+try:
+    from IPython.display import display, HTML
+except Exception as e:
+    raise Exception('IPython not detected. Plotting without IPython is not possible')  # NOQA E501
+
+if 'google.colab' in sys.modules:
+    print("WARNING! You might encounter some issues while running in Google colab environment")  # NOQA E501
+
 from .plotting import (
     plot_class_representations, plot_class_tabs,
     plot_images
 )
+
 import ipyplot.img_helpers
