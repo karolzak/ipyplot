3055b0e3111c6eb0ca974d7863058a8dcdf3960b
Karol Żak <karol.zak@hotmail.com>
2020-05-06 12:25:07 +0200

    Change HTML layout to not rely on JS at all (#2)
    * add tabs without js
    
    * bump version to 1.0.5
    
    * cleaning and minor style fixes
    
    * cosmetics :lipstick:

--- ipyplot/plotting.py
+++ ipyplot/plotting.py
@@ -8,6 +8,11 @@ from PIL import Image
 
 from .img_helpers import resize_with_aspect_ratio
 
+try:
+    from IPython.display import display, HTML
+except Exception as e:
+    raise Exception('IPython not detected. Plotting without IPython is not possible')  # NOQA E501
+
 
 def plot_class_tabs(
         images,
@@ -135,33 +140,61 @@ def plot_class_representations(
         img_width=img_width,
         force_b64=force_b64)
 
-
 def _create_tabs_html(images, labels, max_imgs_per_tab, img_width, force_b64=False):
-    html = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>'  # NOQA E501
-    html = '<div><ul class="nav nav-pills">'
+    # html = '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>'  # NOQA E501
+    html = '<div>'
 
     unique_labels = np.unique(labels)
     tab_ids = [uuid.uuid4() for label in unique_labels]
+    style_html = """
+        <style>
+            input { display: none; }
+            input + label { display: inline-block }
+
+            input + label {
+            border: 1px solid #999;
+            background: #EEE;
+            padding: 4px 12px;
+            border-radius: 4px 4px 0 0;
+            position: relative;
+            top: 1px;
+            }
+            input:checked + label {
+            background: #FFF;
+            border-bottom: 1px solid transparent;
+            }
+            input ~ .tab {
+            border-top: 1px solid #999;
+            padding: 12px;
+            }
+            
+            input ~ .tab { display: none }
+        """
+    
+    for i in tab_ids:
+        style_html += '#tab%s:checked ~ .tab.content%s,' % (i, i)
+    style_html = style_html[:-1] + '{ display: block; }</style>'
+
+    html += style_html
 
     active_tab = True
     for i, label in zip(tab_ids, unique_labels):
-        html += '<li class="active">' if active_tab else '<li>'
+        html += '<input type="radio" name="tabs" id="tab%s"%s/>' % (i, ' checked ' if active_tab else '')  # NOQA E501
+        html += '<label for="tab%s">%s</label>' % (i, label)
         active_tab = False
-        html += '<a href="#%s" data-toggle="tab">%s</a></li>' % (i, label)
-    html += '</ul><div class="tab-content clearfix">'
 
     active_tab = True
     for i, label in zip(tab_ids, unique_labels):
-        html += '<div class="tab-pane active" id="%s">' % i if active_tab else '<div class="tab-pane" id="%s">' % i  # NOQA E501
+        html += '<div class="tab content%s">' % i  # NOQA E501
         active_tab = False
-        img_ids = list(range(0, len(images)))
+
         html += ''.join([
             _create_img_html(x, img_width, label=y, force_b64=force_b64)
-            for x, y in zip(images[labels == label][:max_imgs_per_tab], img_ids)
+            for y, x in enumerate(images[labels == label][:max_imgs_per_tab])
         ])        
         html += '</div>'
 
-    html += '</div></div>'
+    html += '</div>'
 
     return html
 
@@ -184,12 +217,12 @@ def _img_to_base64(image, max_size):
 
 def _create_img_html(image, width, label, force_b64=False):
     html = (
-        '<div style="display: inline-block; width: %spx; text-align: center;">' % width +
+        '<div style="display: inline-block; width: %spx; vertical-align: top; text-align: center;">' % (width + 20) +
         '<h4 style="font-size: 12px">%s</h4>' % label
     )
     use_b64 = True
     if type(image) is str or type(image) is str_:
-        html += '<h4 style="font-size: 9px; padding-left: 15px; padding-right: 15px; width: 100%%; word-wrap: break-word; white-space: normal;">%s</h4>' % (image)  # NOQA E501
+        html += '<h4 style="font-size: 9px; padding-left: 10px; padding-right: 10px; width: 90%%; word-wrap: break-word; white-space: normal;">%s</h4>' % (image)  # NOQA E501
         if not force_b64:
             use_b64 = False
             html += '<img src="%s" style="margin: 1px; width: %spx; border: 2px solid #ddd;"/>' % (image, width)  # NOQA E501
