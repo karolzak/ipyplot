2f200644d0d592d7d23a7a19ac5e7bbedd1b839b
Roger Shieh <sh.rog@protonmail.ch>
2021-02-05 11:56:01 +0800

    adaptive theme

--- ipyplot/_html_helpers.py
+++ ipyplot/_html_helpers.py
@@ -419,12 +419,12 @@ def _get_default_style(img_width: int, zoom_scale: float):
 
         div.ipyplot-content-div-%(0)s {
             width: %(1)spx;
-            background: white;
+            background: var(--jp-layout-color0);
             display: inline-block;
             vertical-align: top;
             text-align: center;
             position: relative;
-            border: 2px solid #ddd;
+            border: var(--jp-border-width) solid var(--jp-cell-editor-border-color);
             top: 0;
             left: 0;
         }
